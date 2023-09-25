3ec78239206e79e6d77a9b2b94f492f104c6450d
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2023-09-25 23:02:19 +0300

    first

--- intro/src/intro/Main.java
+++ intro/src/intro/Main.java
@@ -6,5 +6,37 @@ public class Main {
 		
 		System.out.println("Hello World!");
 		
+		
+		// Değişken isimlendirmeleri camelCase yazılır
+		
+		String  ortaMetin = "İlginizi çekebilir";
+		String  AltMetin = "Vade Süresi";
+		
+		System.out.println(ortaMetin);
+
+		
+		int vade = 12;
+		
+		double dolarDun = 18.14;
+		double dolarBugun = 18.10;
+		
+		boolean dolarDustuMu = false;
+		
+		String okYonu = "";
+		
+		
+		if (dolarBugun < dolarDun) {
+			
+			okYonu = "down.svg";
+			System.out.println(okYonu);
+		} 
+		
+		else {
+			okYonu = "up.svg";
+			System.out.println(okYonu);
+
+		}
+		
+		
 	}
 }
